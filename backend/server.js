885e6d546079589bb6458f99ff50fcc9101b8a0f
Eloagbawe <eloagbawe@gmail.com>
2023-04-11 23:02:55 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const multer = require('multer');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/error');
 const { cloudinaryConfig } = require('./config/cloudinaryConfig');
+const { upload } = require('./services/imageUpload');
 
 const port = process.env.PORT || 5001;
 
@@ -13,11 +13,11 @@ cloudinaryConfig();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({extended: true}));
+app.use(upload);
 app.use('/api/v1/users', require('./routes/userRoutes'));
-app.use('/api/v1/crafts', multer().any(), require('./routes/craftRoutes'));
-app.use('/api/v1/locations', multer().any(), require('./routes/locationRoutes'));
+app.use('/api/v1/crafts', require('./routes/craftRoutes'));
+app.use('/api/v1/locations', require('./routes/locationRoutes'));
 
 
 app.use(errorHandler);
