a850e488a6ef6db017dd447654b239d66dd5c7bb
Eloagbawe <eloagbawe@gmail.com>
2023-04-15 16:10:46 +0100

    update review controller

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -5,6 +5,12 @@ const Artisan = require('../models/artisanModel');
 
 const addReview = asyncHandler(async (req, res) => {
   const id = req.params.id;
+  const { role } = req.user;
+
+  if (role !== 'user') {
+    res.status(401);
+    throw new Error('Not authorized')
+  }
 
   const artisan = await Artisan.findById(id);
 
@@ -13,24 +19,34 @@ const addReview = asyncHandler(async (req, res) => {
     throw new Error('Artisan not found');
   }
 
-  const { text, rating } = req.body;
+  const reviewExists = await Review.findOne({ artisanId: id, userId: req.user._id});
+
+  if (reviewExists) {
+    res.status(400);
+    throw new Error('A review already exists for this artisan');
+  }
+
+  const { text } = req.body;
+  let { rating } = req.body;
 
   if (!text || !rating) {
     res.status(400);
     throw new Error('Please add a text and a rating');
   }
 
-  if ((rating < 0) || (rating > 5)) {
+  rating = Number(rating);
+
+  if ((rating < 0) || (rating > 5) || (!Number.isInteger(rating))) {
     res.status(400);
     throw new Error('Rating must be a number from 0 to 5');
   }
 
   const currentRating = parseFloat(artisan.rating);
   const currentRatingSum = currentRating * artisan.reviews.length;
-  const newRating = (parseFloat(currentRatingSum) + Number(rating)) / (artisan.reviews.length + 1);
+  const newRating = (parseFloat(currentRatingSum) + (rating)) / (artisan.reviews.length + 1);
 
   const review = await Review.create({
-    userId: req.user.id,
+    userId: req.user._id,
     artisanId: id,
     text,
     rating
@@ -40,6 +56,8 @@ const addReview = asyncHandler(async (req, res) => {
     artisan.rating = newRating.toFixed(1);
     artisan.reviews.push(review._id);
     await artisan.save();
+    req.user.reviews.push(review._id);
+    await req.user.save();
     res.status(200).json(review);
   }
 });
@@ -66,18 +84,21 @@ const editReview = asyncHandler(async (req, res) => {
     throw new Error('Not authorized');
   }
 
-  const { text, rating } = req.body;
+  const { text } = req.body;
+  let { rating } = req.body;
 
   let newRating;
 
+  rating = Number(rating);
+
   if (rating) {
-    if ((rating < 0) || (rating > 5)) {
+    if ((rating < 0) || (rating > 5) || (!Number.isInteger(rating))) {
       res.status(400);
       throw new Error('Rating must be a number from 0 to 5');
     } else {
       const currentRating = parseFloat(artisan.rating);
       const currentRatingSum = currentRating * artisan.reviews.length;
-      newRating = ((parseFloat(currentRatingSum) - Number(review.rating)) + Number(rating)) / (artisan.reviews.length);
+      newRating = ((parseFloat(currentRatingSum) - Number(review.rating)) + (rating)) / (artisan.reviews.length);
     }
   }
 
@@ -99,22 +120,33 @@ const deleteReview = asyncHandler(async (req, res) => {
     throw new Error('Review not found')
   }
 
-  if (req.user.id.toString() !== review.userId.toString()) {
+  if (req.user._id.toString() !== review.userId.toString()) {
     res.status(401);
     throw new Error('Not authorized');
   }
 
   const artisan = await Artisan.findById(review.artisanId);
 
+
   if (artisan) {
     const currentRating = parseFloat(artisan.rating);
     const currentRatingSum = currentRating * artisan.reviews.length;
-    const newRating = (parseFloat(currentRatingSum) - Number(review.rating)) / (artisan.reviews.length - 1);
+    const newRating = ((parseFloat(currentRatingSum) - Number(review.rating)) / (artisan.reviews.length - 1)) || 0;
     artisan.rating = newRating;
     const reviewIdIndex = artisan.reviews.indexOf(review._id);
-    artisan.reviews.splice(reviewIdIndex, 1);
-    artisan.save();
+    if (reviewIdIndex >= 0) {
+      artisan.reviews.splice(reviewIdIndex, 1);
+      artisan.save();
+    }
   }
+
+  const userReviewIdIndex = req.user.reviews.indexOf(review._id);
+
+  if (userReviewIdIndex >= 0) {
+    req.user.reviews.splice(userReviewIdIndex, 1);
+    req.user.save();
+  }
+  
   await Review.deleteOne({_id: review._id});
   res.status(200).json({message: `review with id ${id} has been successfully deleted`});
 
