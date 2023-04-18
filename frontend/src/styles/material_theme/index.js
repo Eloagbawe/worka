467d5d4d98d28dcc2ee4fbbe88aafb8f2076be75
Eloagbawe <eloagbawe@gmail.com>
2023-04-18 01:05:44 +0100

    update global styling and add materialui custom theme

---
+++ frontend/src/styles/material_theme/index.js
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+export const THEME = createTheme({
+  typography: {
+    "fontFamily": `"Lato", sans-serif`,
+    "fontSize": 14,
+    "fontWeightLight": 300,
+    "fontWeightRegular": 400,
+    "fontWeightMedium": 500
+  },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+});
