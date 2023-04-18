23a058bffc8b568925e8151dddf4a0397239c2a0
Eloagbawe <eloagbawe@gmail.com>
2023-04-18 01:07:17 +0100

    update _app.js and index.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,26 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react';
+import { addDarkTheme } from '@/_helpers/set_theme';
+import { NavBar } from '@/components/NavBar';
+import { ThemeProvider} from '@mui/material/styles';
+import { THEME } from '@/styles/material_theme'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');  
+    if ((theme === 'dark') || (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)){
+      addDarkTheme();
+    }
+
+  }, []);
+
+ 
+  return (
+    <>
+      <ThemeProvider theme={THEME}>
+        <NavBar/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
