7031dfa68734d960402f77084d0e52e39f2ae4fb
Eloagbawe <eloagbawe@gmail.com>
2023-04-18 01:06:36 +0100

    add favicon and manifest to head tag

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
