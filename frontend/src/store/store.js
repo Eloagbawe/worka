1e109d91c8a8a7e3da4a8ce85088daec3e0317e8
Eloagbawe <eloagbawe@gmail.com>
2023-04-20 23:06:38 +0100

    add redux store

---
+++ frontend/src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import craftReducer from './crafts/craftSlice';
+import locationReducer from './locations/locationSlice';
+import authReducer from './auth/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    craft: craftReducer,
+    location: locationReducer
+  },
+});
