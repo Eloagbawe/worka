87181a2323f76c0d7a5ca057847ce23b10dc30ec
Eloagbawe <eloagbawe@gmail.com>
2023-04-20 23:10:12 +0100

    update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/globals.css'
 import { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 import { addDarkTheme } from '@/_helpers/set_theme';
 import { NavBar } from '@/components/NavBar';
 import { ThemeProvider} from '@mui/material/styles';
 import { THEME } from '@/styles/material_theme';
 import { Footer } from '@/components/Footer';
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -17,14 +24,16 @@ export default function App({ Component, pageProps }) {
 
  
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={THEME}>
+        <ToastContainer autoClose={3000}/>
         <div className='h-screen'>
         <NavBar/>
           <Component {...pageProps} />
         <Footer/>
         </div>
       </ThemeProvider>
-    </>
+    </Provider>
+
   )
 }
