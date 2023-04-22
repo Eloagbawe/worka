cc6cc0419b4ada4f0228da8145be81e1a1b4bb13
Eloagbawe <eloagbawe@gmail.com>
2023-04-22 23:15:15 +0100

    update store.js

--- frontend/src/features/store.js
+++ frontend/src/features/store.js
@@ -4,12 +4,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import craftReducer from './crafts/craftSlice';
 import locationReducer from './locations/locationSlice';
 import authReducer from './auth/authSlice';
+import profileReducer from './profile/profileSlice';
+import bookingReducer from './booking/bookingSlice';
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     craft: craftReducer,
-    location: locationReducer
+    location: locationReducer,
+    profile: profileReducer,
+    booking: bookingReducer
   },
 });
